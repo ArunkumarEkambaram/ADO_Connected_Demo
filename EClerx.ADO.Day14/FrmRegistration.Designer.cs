167f8610ac602a4c8a30f797085e59d5d89a8b41
Arunkumar <arunkumar.e@outlook.com>
2022-07-21 13:20:19 +0530

    Page and Application Level Error

---
+++ EClerx.ADO.Day14/FrmRegistration.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace EClerx.ADO.Day14
+{
+    partial class FrmRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtFirstName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtEmailId = new System.Windows.Forms.TextBox();
+            this.DtpBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.DdlGender = new System.Windows.Forms.ComboBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TxtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.BtnSignIn = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // TxtFirstName
+            // 
+            this.TxtFirstName.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtFirstName.Location = new System.Drawing.Point(285, 92);
+            this.TxtFirstName.Name = "TxtFirstName";
+            this.TxtFirstName.Size = new System.Drawing.Size(222, 31);
+            this.TxtFirstName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(227, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Registration";
+            // 
+            // TxtLastName
+            // 
+            this.TxtLastName.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtLastName.Location = new System.Drawing.Point(285, 150);
+            this.TxtLastName.Name = "TxtLastName";
+            this.TxtLastName.Size = new System.Drawing.Size(222, 31);
+            this.TxtLastName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(111, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(111, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Birth Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(111, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 25);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Gender";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(111, 332);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Email Id";
+            // 
+            // TxtEmailId
+            // 
+            this.TxtEmailId.AccessibleName = "Email Id";
+            this.TxtEmailId.AccessibleRole = System.Windows.Forms.AccessibleRole.Text;
+            this.TxtEmailId.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtEmailId.Location = new System.Drawing.Point(285, 325);
+            this.TxtEmailId.Name = "TxtEmailId";
+            this.TxtEmailId.Size = new System.Drawing.Size(222, 31);
+            this.TxtEmailId.TabIndex = 10;
+            // 
+            // DtpBirthDate
+            // 
+            this.DtpBirthDate.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DtpBirthDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DtpBirthDate.Location = new System.Drawing.Point(285, 208);
+            this.DtpBirthDate.Name = "DtpBirthDate";
+            this.DtpBirthDate.Size = new System.Drawing.Size(222, 31);
+            this.DtpBirthDate.TabIndex = 11;
+            this.DtpBirthDate.ValueChanged += new System.EventHandler(this.DtpBirthDate_ValueChanged);
+            // 
+            // DdlGender
+            // 
+            this.DdlGender.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DdlGender.FormattingEnabled = true;
+            this.DdlGender.Location = new System.Drawing.Point(285, 266);
+            this.DdlGender.Name = "DdlGender";
+            this.DdlGender.Size = new System.Drawing.Size(222, 33);
+            this.DdlGender.TabIndex = 12;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtPassword.Location = new System.Drawing.Point(285, 383);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(222, 31);
+            this.TxtPassword.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(111, 390);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Password";
+            // 
+            // TxtConfirmPassword
+            // 
+            this.TxtConfirmPassword.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtConfirmPassword.Location = new System.Drawing.Point(285, 441);
+            this.TxtConfirmPassword.Name = "TxtConfirmPassword";
+            this.TxtConfirmPassword.Size = new System.Drawing.Size(222, 31);
+            this.TxtConfirmPassword.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(111, 448);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(156, 25);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Confirm Password";
+            // 
+            // BtnSignIn
+            // 
+            this.BtnSignIn.AutoSize = true;
+            this.BtnSignIn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BtnSignIn.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSignIn.Location = new System.Drawing.Point(115, 539);
+            this.BtnSignIn.Name = "BtnSignIn";
+            this.BtnSignIn.Size = new System.Drawing.Size(112, 33);
+            this.BtnSignIn.TabIndex = 17;
+            this.BtnSignIn.Text = "Sign In";
+            this.BtnSignIn.UseVisualStyleBackColor = false;
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.AutoSize = true;
+            this.BtnReset.BackColor = System.Drawing.Color.Red;
+            this.BtnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnReset.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnReset.ForeColor = System.Drawing.Color.White;
+            this.BtnReset.Location = new System.Drawing.Point(395, 539);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(112, 33);
+            this.BtnReset.TabIndex = 18;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = false;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // FrmRegistration
+            // 
+            this.AcceptButton = this.BtnSignIn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.CancelButton = this.BtnReset;
+            this.ClientSize = new System.Drawing.Size(619, 647);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.BtnSignIn);
+            this.Controls.Add(this.TxtConfirmPassword);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.DdlGender);
+            this.Controls.Add(this.DtpBirthDate);
+            this.Controls.Add(this.TxtEmailId);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtLastName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtFirstName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmRegistration";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registration";
+            this.Load += new System.EventHandler(this.FrmRegistration_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtLastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtEmailId;
+        private System.Windows.Forms.DateTimePicker DtpBirthDate;
+        private System.Windows.Forms.ComboBox DdlGender;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox TxtConfirmPassword;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button BtnSignIn;
+        private System.Windows.Forms.Button BtnReset;
+    }
+}
---
+++ EClerx.ADO.Day14/FrmRegistration.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EClerx.ADO.Day14
+{
+    public partial class FrmRegistration : Form
+    {
+        public FrmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearAll()
+        {
+            TxtFirstName.Text = "";
+            TxtLastName.Text = "";
+            TxtEmailId.Text = "";
+            TxtPassword.Text = "";
+            TxtConfirmPassword.Text = "";
+            DdlGender.Text = "--Select--";
+            DtpBirthDate.Value = DateTime.Now;
+        }
+
+        private int FindAge()
+        {
+            DateTime selectedDate = DtpBirthDate.Value;
+            int age = DateTime.Now.Year - selectedDate.Year;
+            if (DateTime.Now.DayOfYear < selectedDate.DayOfYear)
+            {
+                age--;
+            }
+            return age;
+        }
+
+        private void FrmRegistration_Load(object sender, EventArgs e)
+        {
+            DdlGender.Text = "--Select--";
+            DdlGender.Items.Add("Male");
+            DdlGender.Items.Add("Female");
+            DdlGender.Items.Add("Others");
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            this.ClearAll();
+        }
+
+        private void DtpBirthDate_ValueChanged(object sender, EventArgs e)
+        {
+            int age = FindAge();
+            if (age < 18)
+            {
+                MessageBox.Show("Age should be greater than 18 years");                
+            }          
+        }
+    }
+}
